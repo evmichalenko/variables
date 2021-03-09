d08c4c9980e96f25f2f2654b1c309256ae7da5ce
evmichalenko <701721@smcdsb.on.ca>
2021-03-09 20:43:26 +0000

    Variables project

---
+++ variables.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>  
+
+int main(void)
+{
+    int age = get_int("Enter your age: ");
+    printf("Right now I'm %i years old.\n", age);
+    
+    // My age next year
+    printf("Next year I'll be %i years old.\n", age + 1);
+    
+    // My age in 10 years
+    printf("In 10 years, I'll be %i!\n", age + 10);
+    
+    // My age in 50 years!
+    printf("In 50 years, I'll be %i! Wow!\n", age + 50);
+}
